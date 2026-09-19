3cf1fe8a30a2beccb079f826ce940213a6546a62
Yevhen Sidelnyk <zsidelnik@gmail.com>
2026-09-19 10:19:56 +0300

    wip

--- rector.php
+++ rector.php
@@ -3,17 +3,31 @@
 declare(strict_types=1);
 
 use PhPhD\CodingStandard\ValueObject\Set\PhdSetList;
+use Rector\CodeQuality\Rector\Attribute\ExplicitAttributeNamedArgsRector;
+use Rector\CodeQuality\Rector\CallLike\AddNameToBooleanArgumentRector;
+use Rector\CodeQuality\Rector\CallLike\AddNameToNullArgumentRector;
 use Rector\CodeQuality\Rector\ClassMethod\LocallyCalledStaticMethodToNonStaticRector;
 use Rector\CodeQuality\Rector\If_\SimplifyIfElseToTernaryRector;
 use Rector\CodeQuality\Rector\Isset_\IssetOnPropertyObjectToPropertyExistsRector;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveDuplicatedReturnSelfDocblockRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveParentDelegatingClassMethodRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
 use Rector\DeadCode\Rector\If_\ReduceAlwaysFalseIfOrRector;
 use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
+use Rector\Naming\Rector\Foreach_\RenameForeachValueVariableToMatchExprVariableRector;
+use Rector\Naming\Rector\Foreach_\RenameForeachValueVariableToMatchMethodCallReturnTypeRector;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
+use Rector\Php80\Rector\Catch_\RemoveUnusedVariableInCatchRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Php81\Rector\Array_\ArrayToFirstClassCallableRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitSelfCallRector;
+use Rector\PHPUnit\CodeQuality\Rector\Expression\DecorateWillReturnMapWithExpectsMockRector;
+use Rector\Symfony\Configs\Rector\Closure\MergeServiceNameTypeRector;
+use Rector\Symfony\Configs\Rector\Closure\ServiceArgsToServiceNamedArgRector;
+use Rector\Symfony\Configs\Rector\Closure\ServiceTagsToDefaultsAutoconfigureRector;
 use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
@@ -22,16 +36,18 @@ return RectorConfig::configure()
     ->withPhpVersion(PhpVersion::PHP_81)
     ->withSkip([
         ...stubExclusions(),
-        SimplifyIfElseToTernaryRector::class,
         ArrayToFirstClassCallableRector::class => [
             __DIR__.'/src/*/services.php',
             __DIR__.'/src/*/*CompilerPass.php',
         ],
-        ClassPropertyAssignToConstructorPromotionRector::class => [
-            __DIR__.'/src/ExceptionalMatcher/Exception/ExceptionReciprocal.php',
-        ],
         IssetOnPropertyObjectToPropertyExistsRector::class => [
-            __DIR__.'/src/ExceptionalMatcher/Rule/Object/Property/Match/Condition/Origin/ExceptionOriginMatchCondition.php',
+            __DIR__.'/'.'src/ExceptionalMatcher/Mapping/Object/Property/Catch_/Condition/Origin/ExceptionOriginMatchCondition.php',
+        ],
+        RemoveParentDelegatingClassMethodRector::class => [
+            __DIR__.'/'.'src/ExceptionalMatcher/Integration/Validator/Middleware/Messenger/ExceptionalValidationFailedMessengerException.php',
+        ],
+        ClassPropertyAssignToConstructorPromotionRector::class => [
+            __DIR__.'/'.'src/ExceptionalMatcher/Mapping/Object/Property/Catch_/Exception/ExceptionReciprocal.php',
         ],
         StringClassNameToClassConstantRector::class => [
             __DIR__.'/upgrade',
@@ -39,7 +55,15 @@ return RectorConfig::configure()
         ReduceAlwaysFalseIfOrRector::class => [
             __DIR__.'/upgrade',
         ],
-    ]);
+    ])
+    ->withSkip([ /// FIXME: process this list, removing the rules one by one:
+//        ExplicitAttributeNamedArgsRector::class,
+//        AddNameToBooleanArgumentRector::class,
+//        AddNameToNullArgumentRector::class,
+//        DecorateWillReturnMapWithExpectsMockRector::class,
+//        NewlineAfterStatementRector::class,
+    ])
+    ;
 
 function stubExclusions(): array
 {
